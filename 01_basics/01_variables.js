8f54162f3d31b146e71d6df198ceec231de830ca
Rahul-9353 <rahulsherigar085@gmail.com>
2024-10-15 15:49:29 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const account_ID = 144553
+let account_Email = "rahul@google.com"
+var account_Password = "12345"
+account_City = "Udupi"
+let account_State
+
+// account_ID = 2  not allowed
+
+account_Email = "qw@google.com"
+account_Password = "111111"
+account_City = "Mangalore"
+
+console.log(account_ID);
+
+console.table([account_ID,account_Email,account_Password,account_City,account_State])
