1a034a2a986f44808805d7af9e4db52c3897c220
Rahul-9353 <rahulsherigar085@gmail.com>
2024-10-21 10:08:32 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) we are using nodejs,not browser
+
+console.log(3+3)
+                    // code readability
+console.log("Rahul")
+
+
+let name="Rahul"
+let age=23
+let isLoggedIn=false
+let state=null
+
+// number =>2 pwr 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefine =>unique
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // null
