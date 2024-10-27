aecf66beba84e370cc249c873abdb6e887221faf
Rahul-9353 <rahulsherigar085@gmail.com>
2024-10-27 10:20:00 +0000

    scope2

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,11 +3,61 @@ if (true) {
     let a = 10
     const b = 20
     // var c = 30
-    console.log("INNER: ",a);
+    // console.log("INNER: ",a);
     
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+
+function one() {
+    const username = "rahul"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+
+    // two()
+    
+}
+
+// one()
+
+
+if (true) {
+    const username = "rahul"
+    if (username === "rahul") {
+        const website = "youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+
+
+
+// +++++++++++++++++++++++++++++ intresting +++++++++++++++++++++++++++++++
+
+
+function addone(num) {
+    return num + 1
+}
+
+console.log(addone(5))
+
+addTwo(5)
+
+const addTwo = function(num){
+    return num + 2
+}
